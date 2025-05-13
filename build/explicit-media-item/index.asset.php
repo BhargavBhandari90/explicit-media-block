2a9448bdb58a170840b50957d9942188ebda68d1
Bunty <bn.bhandari90@gmail.com>
2025-05-13 17:32:01 +0530

    Add Picture in Picture button on hover of the video

--- build/explicit-media-item/index.asset.php
+++ build/explicit-media-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7fea12cb4589fb4da2f2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '182e3b8010e0c074dcbc');
--- build/explicit-media-item/view.asset.php
+++ build/explicit-media-item/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '457943e45c26e9eafd2b', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '800d4f0c21e217dc08dd', 'type' => 'module');
